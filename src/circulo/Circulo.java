2f21d9202ffb2c1a194e0a67859e41fb257ea61a
frann98 <franache69@gmail.com>
2022-02-24 09:35:39 +0100

    prueba 02 commit circulo

--- src/circulo/Circulo.java
+++ src/circulo/Circulo.java
@@ -1,28 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package circulo;
 import java.util.Scanner;
 
-/**
- *
- * @author monse
- */
 public class Circulo {
 
     final static double PI=3.1416;
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
         
+        try{
         Scanner s=new Scanner(System.in);
         System.out.println("Introduce el radio:");
         double r=s.nextDouble();
         CalculaArea(r);
+        } catch(Exception e){System.out.println("error");}
         
     }
     public static void CalculaArea(double r) {
